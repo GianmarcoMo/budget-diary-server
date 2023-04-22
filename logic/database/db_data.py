4fc92d3f0b53da2d87b9fb72db4fbc093af8b2a0
Gianmarco Moresi <gianmarco.moresi@yahoo.it>
2023-04-22 15:53:05 +0200

    delete print

--- logic/database/db_data.py
+++ logic/database/db_data.py
@@ -6,9 +6,6 @@ def get_all_movements(collection):
     result_query = collection.find().sort("date", pymongo.DESCENDING)
 
     for movement in result_query:
-        print(movement)
         movements.append(movement)
-
-    print(movements)
 
     return movements
